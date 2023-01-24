f98f8da3acb8d92325ef9a1d8ad93320b0cd2b18
Dharani8838 <90498910+Dharani8838@users.noreply.github.com>
2023-01-24 19:15:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,10 +49,10 @@ CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "ɴᴀᴍᴇ: <code>{fi
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", "ɴᴀᴍᴇ: <code>{file_name}</code> \n\nJOIN NOW: [© @DailyMvTamil](https://t.me/dailymvtamil)</b>")
 CUSTOM_FILE_IMAGE = environ.get ("CUSTOM_FILE_IMAGE", "IMAGE <code>https://telegra.ph/file/b3d4320131af3222e4016.jpg</code> </b>")
 IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "🎟 𝐓𝐢𝐭𝐥𝐞 : {title} \n📆 𝐑𝐞𝐥𝐞𝐚𝐬𝐞 : {year} \n𝐆𝐞𝐧𝐫𝐞𝐬 : {genres} \n✨ Rating : {rating} IMDB\n
-                            ©️ 𝐔𝐏𝐋𝐎𝐀𝐃 𝐁𝐘: @DailyMvTamil")
+©️ 𝐔𝐏𝐋𝐎𝐀𝐃 𝐁𝐘: @DailyMvTamil")
 LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
 SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
-MAX_LIST_ELM = environ.get("MAX_LIST_ELM", True)
+MAX_LIST_ELM = environ.get("MAX_LIST_ELM",else)
 INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
 FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).split()]
 MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
