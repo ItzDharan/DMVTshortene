f3bab126f0b5b1990af0c3f50a9c2f587d8d0f87
ItzDharan <90498910+ItzDharan@users.noreply.github.com>
2024-03-23 15:49:48 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -1,4 +1,3 @@
-#https://github.com/AM-ROBOTS
 from pyrogram import Client, filters
 import datetime
 import time
@@ -6,10 +5,9 @@ from database.users_chats_db import db
 from info import ADMINS
 from utils import broadcast_messages
 import asyncio
-        
+
 @Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
-# https://t.me/GetTGLink/4178
-async def verupikkals(bot, message):
+async def broadcast_to_users(bot, message):
     users = await db.get_all_users()
     b_msg = message.reply_to_message
     sts = await message.reply_text(
@@ -20,23 +18,29 @@ async def verupikkals(bot, message):
     done = 0
     blocked = 0
     deleted = 0
-    failed =0
-
+    failed = 0
     success = 0
+    
     async for user in users:
-        pti, sh = await broadcast_messages(int(user['id']), b_msg)
-        if pti:
-            success += 1
-        elif pti == False:
-            if sh == "Blocked":
-                blocked+=1
-            elif sh == "Deleted":
-                deleted += 1
-            elif sh == "Error":
-                failed += 1
-        done += 1
-        await asyncio.sleep(2)
-        if not done % 20:
-            await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
-    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
+        try:
+            pti, sh = await broadcast_messages(int(user['id']), b_msg)
+            if pti:
+                success += 1
+            elif pti == False:
+                if sh == "Blocked":
+                    blocked += 1
+                elif sh == "Deleted":
+                    deleted += 1
+                elif sh == "Error":
+                    failed += 1
+            done += 1
+            await asyncio.sleep(2)  # Rate limiting
+            if not done % 10:  # Update progress every 10 users
+                await sts.edit(f"Broadcast in progress:\n\nTotal Users: {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}\nFailed: {failed}")
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+            failed += 1
+            continue
+
+    time_taken = datetime.timedelta(seconds=int(time.time() - start_time))
+    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users: {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}\nFailed: {failed}")
